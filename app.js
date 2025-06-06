c737467bd5ebda05b4ac4e0a319b1e6003a30e82
Ludwig Panduro Als <lals@itu.dk>
2025-06-06 19:59:25 +0200

    tech, index and app fixed

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+// double click just reload page. For instance on "Tech"
+document.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    const href = link.getAttribute("href");
+    const current = window.location.pathname.split("/").pop();
+    if (href === current) {
+      e.preventDefault();
+      window.location.href = href;
+    }
+  });
+});
